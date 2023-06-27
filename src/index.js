5f69c52c84a3d9bb95fb8e1acfd84c8996494420
Daeha Ko <daehako@gmail.com>
2023-06-27 01:07:59 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import NameTag from "./NameTag.js";
+
+import "./styles.css";
+
+const renderNameTag = (name) => <NameTag name={name} key={name} />;
+
+class App extends Component {
+  state = { names: ["Daeha", "John", "Paul", "George", "Ringo"] };
+
+  render() {
+    const NameTagElements = this.state.names.map(renderNameTag);
+    return (
+      <div className="App">
+        <h1>Name Tag Generator</h1>
+        {NameTagElements}
+      </div>
+    );
+  }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
